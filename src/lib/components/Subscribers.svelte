af723c61c862baa2d415f09435bd88d0964b2c84
sbenitomartin <sergiobm96@gmail.com>
2024-01-06 23:30:40 +0100

    fix

--- src/lib/components/Subscribers.svelte
+++ src/lib/components/Subscribers.svelte
@@ -1,4 +1,5 @@
-<a class="link-light link-underlined" style="font-size: 0.95rem;" href="#subscribeModal" data-toggle="modal" data-target="#subscribeModal">Do you want to be notified of new posts?</a>
+<!-- You have to add a modal opener whenever you want. Example below -->
+<!-- <a class="link-light link-underlined" style="font-size: 0.95rem;" href="#subscribeModal" data-toggle="modal" data-target="#subscribeModal">Do you want to be notified of new posts?</a> -->
 
 <!-- Modal -->
 <div id="subscribeModal" class="modal fade" role="dialog" style="color: black">
@@ -9,9 +10,7 @@
         <button type="button" class="close" data-dismiss="modal">&times;</button>
       </div>
       <div class="modal-body">
-        test
-        <!-- I had to remove async fomr <script async ...> int orther to work when svelte doesn't reload the whole page but it changes de SPA content -->
-        <script src="https://eocampaign1.com/form/57a89816-abf6-11ee-892b-7ffba58ed840.js" data-form="57a89816-abf6-11ee-892b-7ffba58ed840"></script>
+        <script async src="https://eocampaign1.com/form/57a89816-abf6-11ee-892b-7ffba58ed840.js" data-form="57a89816-abf6-11ee-892b-7ffba58ed840"></script>
       </div>
     </div>
   </div>
--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -42,7 +42,8 @@
 	<div class="row gx-4 gx-lg-5 justify-content-center">
 		<div class="col-md-10 col-lg-8 col-xl-7">
 			<ul class="list-inline text-center">
-				<Subscribers/>
+				<!-- Subscribers modal opener -->
+				<a class="link-light link-underlined" style="font-size: 0.95rem;" href="#subscribeModal" data-toggle="modal" data-target="#subscribeModal">Do you want to be notified of new posts?</a>
 			</ul>
 		</div>
 	</div>
