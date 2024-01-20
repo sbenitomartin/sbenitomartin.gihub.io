2f5d0250d17f8df0f2f39f2ced0a31a5448ce6f3
sbenitomartin <sergiobm96@gmail.com>
2024-01-20 21:30:33 +0100

    added nav_custom_sm

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -5,7 +5,7 @@
 
 	function switchTheme(cookie = true) {
 		var theme = '';
-		var dark = document.body.classList.contains("dark");
+		var dark = document.body.classList.contains('dark');
 		if (dark === false) {
 			theme = 'dark';
 			document.body.classList.remove('light');
@@ -30,7 +30,7 @@
 
 <!-- <nav class="navbar navbar-expand-lg shadow-none navbar-light" id="mainNav">
 	<div class="container px-4 px-lg-5">
-		<a class="navbar-brand" href="{base}/">{blog_title}</a>
+		<a class="navbar-brand" href="{base}/">aaa</a>
 		<button
 			class="navbar-toggler collapsed"
 			type="button"
@@ -55,17 +55,85 @@
 	</div>
 </nav> -->
 
+<div class="">
+	<!-- <a rel="nav_custom_sm" style="top: 1.5rem; right: 1.5rem; display: none;"> ︎ </a> -->
+
+	<div class="ms-auto py-1 px-2 nav-custom d-block d-sm-none" style="font-size: 1.10rem;">
+		<button
+			class="nav-custom nav-icon-custom navbar-toggler collapsed d-sm-none"
+			style="font-size: 2.50rem; right: 0; top: 0;"
+			type="button"
+			data-bs-toggle="collapse"
+			data-bs-target="#navbarResponsive"
+			aria-controls="navbarResponsive"
+			aria-expanded="false"
+			aria-label="Toggle navigation"><i class="fas fa-plus" /></button
+		>
+	</div>
+
+	<div class="d-block d-sm-none">
+		<div class="collapse navbar-collapse" id="navbarResponsive">
+			<div class="nav_custom_sm">
+				<div class="page-link" data-link-pid="19794400" data-prevtype="">
+					<a class="nav-link-custom" href="{base}/">Home</a>
+				</div>
+				<div class="page-link" data-link-pid="18415401" data-prevtype="">
+					<a class="nav-link-custom" href="{base}/about">About</a>
+				</div>
+				<div class="page-link" data-link-pid="15819705" data-prevtype="">
+					<a class="nav-link-custom" href="{base}/contact">Contact</a>
+				</div>
+				<div class="page-link" data-link-pid="15819705" data-prevtype="">
+					<button
+						class="nav-custom nav-icon-custom"
+						style="font-size: 2.50rem;"
+						on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
+					>
+				</div>
+
+				<button
+					class="close nav-custom nav-icon-custom navbar-toggler"
+					style="font-size: 2.50rem;"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarResponsive"
+					aria-controls="navbarResponsive"
+					aria-expanded="false"
+					aria-label="Toggle navigation"><i class="fas fa-xmark" /></button
+				>
+			</div>
+		</div>
+	</div>
+</div>
+
 <nav>
 	<div class="ms-auto py-1 px-2 nav-custom d-none d-sm-block">
-		<b><a class="nav-link-custom" href="{base}/">Home</a></b> | <i><a class="nav-link-custom" href="{base}/about">About</a></i> | <a class="nav-link-custom" href="{base}/contact">Contact</a> <button class="nav-custom nav-icon-custom" on:click={() => switchTheme()} ><i class="fas fa-circle-half-stroke" /></button>
+		<b><a class="nav-link-custom" href="{base}/">Home</a></b> |
+		<i><a class="nav-link-custom" href="{base}/about">About</a></i>
+		| <a class="nav-link-custom" href="{base}/contact">Contact</a>
+		<button class="nav-custom nav-icon-custom" on:click={() => switchTheme()}
+			><i class="fas fa-circle-half-stroke" /></button
+		>
 	</div>
-	<div class="ms-auto py-1 px-2 nav-custom d-block d-sm-none" style="font-size: 1.10rem; text-align: center;">
-		<b><a class="nav-link-custom" href="{base}/">Home</a></b> | <i><a class="nav-link-custom" href="{base}/about">About</a></i> | <a class="nav-link-custom" href="{base}/contact">Contact</a> <button class="nav-custom nav-icon-custom" style="font-size: 1.10rem;" on:click={() => switchTheme()} ><i class="fas fa-circle-half-stroke" /></button>
+
+	<!-- OLD SM NAV remove d-none to show-->
+	<div
+		class="ms-auto py-1 px-2 nav-custom d-block d-sm-none d-none"
+		style="font-size: 1.10rem; text-align: center;"
+	>
+		<b><a class="nav-link-custom" href="{base}/">Home</a></b> |
+		<i><a class="nav-link-custom" href="{base}/about">About</a></i>
+		| <a class="nav-link-custom" href="{base}/contact">Contact</a>
+		<button
+			class="nav-custom nav-icon-custom"
+			style="font-size: 1.10rem;"
+			on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
+		>
 	</div>
 </nav>
 
 <slot />
 
-<Footer/>
+<Footer />
 
-<Subscribers/>
+<Subscribers />
