9ae7186aede440ca72ebade0e4205d99f7d3c3d5
sbenitomartin <sergiobm96@gmail.com>
2024-01-21 02:24:25 +0100

    added nav_custom_sm

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -3,6 +3,8 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import Subscribers from '$lib/components/Subscribers.svelte';
 
+	let nav_custom_sm_close_hidden = false;
+
 	function switchTheme(cookie = true) {
 		var theme = '';
 		var dark = document.body.classList.contains('dark');
@@ -19,6 +21,15 @@
 		if (cookie) {
 			const one_year = 60 * 60 * 24 * 365;
 			document.cookie = `theme=${theme}; max-age=${one_year}; path=/`;
+		}
+	}
+
+	function bodyNoScroll() {
+		var body_no_scroll = document.body.classList.contains('body_no_scroll');
+		if (body_no_scroll === false) {
+			document.body.classList.add('body_no_scroll');
+		} else {
+			document.body.classList.remove('body_no_scroll');
 		}
 	}
 </script>
@@ -55,12 +66,14 @@
 	</div>
 </nav> -->
 
-<div class="">
-	<!-- <a rel="nav_custom_sm" style="top: 1.5rem; right: 1.5rem; display: none;"> ︎ </a> -->
-
-	<div class="ms-auto py-1 px-2 nav-custom d-block d-sm-none" style="font-size: 1.10rem;">
+<!-- NEW SM NAV remove d-none to show-->
+<div class="d-block d-sm-none d-none">
+	<div class="ms-auto py-1 px-2 nav-custom">
 		<button
-			class="nav-custom nav-icon-custom navbar-toggler collapsed d-sm-none"
+			class:invisible={nav_custom_sm_close_hidden}
+			on:click="{() => nav_custom_sm_close_hidden = !nav_custom_sm_close_hidden}"
+			on:click={() => bodyNoScroll()}
+			class="nav_custom_sm_open nav-custom nav-icon-custom navbar-toggler collapsed d-sm-none"
 			style="font-size: 2.50rem; right: 0; top: 0;"
 			type="button"
 			data-bs-toggle="collapse"
@@ -71,37 +84,37 @@
 		>
 	</div>
 
-	<div class="d-block d-sm-none">
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-			<div class="nav_custom_sm">
-				<div class="page-link" data-link-pid="19794400" data-prevtype="">
-					<a class="nav-link-custom" href="{base}/">Home</a>
-				</div>
-				<div class="page-link" data-link-pid="18415401" data-prevtype="">
-					<a class="nav-link-custom" href="{base}/about">About</a>
-				</div>
-				<div class="page-link" data-link-pid="15819705" data-prevtype="">
-					<a class="nav-link-custom" href="{base}/contact">Contact</a>
-				</div>
-				<div class="page-link" data-link-pid="15819705" data-prevtype="">
-					<button
-						class="nav-custom nav-icon-custom"
-						style="font-size: 2.50rem;"
-						on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
-					>
-				</div>
-
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<div class="nav_custom_sm">
+			<div>
+				<a class="nav-link-custom" href="{base}/">Home</a>
+			</div>
+			<div>
+				<a class="nav-link-custom" href="{base}/about">About</a>
+			</div>
+			<div>
+				<a class="nav-link-custom" href="{base}/contact">Contact</a>
+			</div>
+			<div>
 				<button
-					class="close nav-custom nav-icon-custom navbar-toggler"
+					class="nav-custom nav-icon-custom"
 					style="font-size: 2.50rem;"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarResponsive"
-					aria-controls="navbarResponsive"
-					aria-expanded="false"
-					aria-label="Toggle navigation"><i class="fas fa-xmark" /></button
+					on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
 				>
 			</div>
+
+			<button
+				on:click="{() => nav_custom_sm_close_hidden = !nav_custom_sm_close_hidden}"
+				on:click={() => bodyNoScroll()}
+				class="nav_custom_sm_close nav-custom nav-icon-custom navbar-toggler collapsed"
+				style="font-size: 2.50rem;"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#navbarResponsive"
+				aria-controls="navbarResponsive"
+				aria-expanded="false"
+				aria-label="Toggle navigation"><i class="fas fa-xmark" /></button
+			>
 		</div>
 	</div>
 </div>
@@ -118,7 +131,7 @@
 
 	<!-- OLD SM NAV remove d-none to show-->
 	<div
-		class="ms-auto py-1 px-2 nav-custom d-block d-sm-none d-none"
+		class="ms-auto py-1 px-2 nav-custom d-block d-sm-none"
 		style="font-size: 1.10rem; text-align: center;"
 	>
 		<b><a class="nav-link-custom" href="{base}/">Home</a></b> |
