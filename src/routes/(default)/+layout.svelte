20488aac20237cd11039b70e8585a94157f705e0
sergiobenito <sergiobm96@gmail.com>
2024-01-26 21:43:45 +0100

    enable new navbar sm

--- src/routes/(default)/+layout.svelte
+++ src/routes/(default)/+layout.svelte
@@ -2,6 +2,7 @@
 	import { base } from '$app/paths';
 	import Footer from '$lib/components/Footer.svelte';
 	import Subscribers from '$lib/components/Subscribers.svelte';
+	import { page } from '$app/stores'
 
 	let nav_custom_sm_close_hidden = false;
 
@@ -43,113 +44,81 @@
 
 <svelte:window on:resize={dispatchResize} />
 
-<!-- <nav>
-	<a href="{base}/">home</a>
-	<a href="{base}/blog">posts list</a>
-</nav> -->
-
-<!-- <nav class="navbar navbar-expand-lg shadow-none navbar-light" id="mainNav">
-	<div class="container px-4 px-lg-5">
-		<a class="navbar-brand" href="{base}/">aaa</a>
-		<button
-			class="navbar-toggler collapsed"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarResponsive"
-			aria-controls="navbarResponsive"
-			aria-expanded="false"
-			aria-label="Toggle navigation"
-		>
-			<i class="fas fa-bars" />
-		</button>
-		<div class="collapse navbar-collapse" id="navbarResponsive">
-			<ul class="navbar-nav ms-auto py-4 py-lg-0">
-				<li class="nav-item">
-					<a class="nav-link px-lg-3 py-3 py-lg-4" href="{base}/about">About</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link px-lg-3 py-3 py-lg-4" href="{base}/contact">Contact</a>
-				</li>
-			</ul>
-		</div>
-	</div>
-</nav> -->
-
-<!-- NEW SM NAV remove d-none to show-->
-<div class="d-block d-sm-none">
-	<div style="top :5px; right: 5px; position: fixed; z-index: 1030;">
-		<button
-			class:invisible={nav_custom_sm_close_hidden}
-			on:click="{() => nav_custom_sm_close_hidden = !nav_custom_sm_close_hidden}"
-			on:click={() => bodyNoScroll()}
-			class="nav_custom_sm_open nav-custom nav-icon-custom navbar-toggler collapsed d-sm-none"
-			style="font-size: 2.50rem; right: 0; top: 0;"
-			type="button"
-			data-bs-toggle="collapse"
-			data-bs-target="#navbarResponsive"
-			aria-controls="navbarResponsive"
-			aria-expanded="false"
-			aria-label="Toggle navigation"><i class="fas fa-plus" /></button
-		>
-	</div>
-
-	<div class="collapse navbar-collapse" id="navbarResponsive">
-		<div class="nav_custom_sm">
-			<div>
-				<a class="nav-link-custom" href="{base}/">Home</a>
-			</div>
-			<div>
-				<a class="nav-link-custom" href="{base}/about">About</a>
-			</div>
-			<div>
-				<a class="nav-link-custom" href="{base}/contact">Contact</a>
-			</div>
-			<div>
-				<button
-					class="nav-custom nav-icon-custom"
-					style="font-size: 2.50rem;"
-					on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
-				>
-			</div>
-
+<!-- SM NAV -->
+<nav>
+	<div class="d-block d-sm-none">
+		<div style="top :5px; right: 5px; position: fixed; z-index: 1030;">
 			<button
+				class:invisible={nav_custom_sm_close_hidden}
 				on:click="{() => nav_custom_sm_close_hidden = !nav_custom_sm_close_hidden}"
 				on:click={() => bodyNoScroll()}
-				class="nav_custom_sm_close nav-custom nav-icon-custom navbar-toggler collapsed"
-				style="font-size: 2.50rem;"
+				class="nav_custom_sm_open nav-custom nav-icon-custom navbar-toggler collapsed d-sm-none"
+				style="font-size: 2.50rem; right: 0; top: 0;"
 				type="button"
 				data-bs-toggle="collapse"
 				data-bs-target="#navbarResponsive"
 				aria-controls="navbarResponsive"
 				aria-expanded="false"
-				aria-label="Toggle navigation"><i class="fas fa-xmark" /></button
+				aria-label="Toggle navigation"><i class="fas fa-plus" /></button
 			>
 		</div>
-	</div>
-</div>
 
+		<div class="collapse navbar-collapse" id="navbarResponsive" style="position: fixed; z-index: 1030;">
+			<div class="nav_custom_sm">
+				{#if $page.url.pathname !== '/'}
+				<div>
+					<a class="nav-link-custom" href="{base}/">Home</a>
+				</div>
+				{/if}
+				{#if $page.url.pathname !== '/blog'}
+				<div>
+					<a class="nav-link-custom" href="{base}/blog">List</a>
+				</div>
+				{/if}
+				<div>
+					<a class="nav-link-custom" href="{base}/about">About</a>
+				</div>
+				<div>
+					<a class="nav-link-custom" href="{base}/contact">Contact</a>
+				</div>
+				<div>
+					<button
+						class="nav-custom nav-icon-custom"
+						style="font-size: 2.50rem;"
+						on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
+					>
+				</div>
+
+				<button
+					on:click="{() => nav_custom_sm_close_hidden = !nav_custom_sm_close_hidden}"
+					on:click={() => bodyNoScroll()}
+					class="nav_custom_sm_close nav-custom nav-icon-custom navbar-toggler collapsed"
+					style="font-size: 2.50rem;"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarResponsive"
+					aria-controls="navbarResponsive"
+					aria-expanded="false"
+					aria-label="Toggle navigation"><i class="fas fa-xmark" /></button
+				>
+			</div>
+		</div>
+	</div>
+</nav>
+
+<!-- DEFAULT NAV -->
 <nav>
 	<div class="ms-auto py-1 px-2 nav-custom d-none d-sm-block">
+		{#if $page.url.pathname !== '/'}
 		<b><a class="nav-link-custom" href="{base}/">Home</a></b> |
+		{/if}
+		{#if $page.url.pathname !== '/blog'}
+		<b><a class="nav-link-custom" href="{base}/blog">List</a></b> |
+		{/if}
 		<i><a class="nav-link-custom" href="{base}/about">About</a></i>
 		| <a class="nav-link-custom" href="{base}/contact">Contact</a>
 		<button class="nav-custom nav-icon-custom" on:click={() => switchTheme()}
 			><i class="fas fa-circle-half-stroke" /></button
-		>
-	</div>
-
-	<!-- OLD SM NAV remove d-none to show-->
-	<div
-		class="ms-auto py-1 px-2 nav-custom d-block d-sm-none d-none"
-		style="font-size: 1.10rem; text-align: center;"
-	>
-		<b><a class="nav-link-custom" href="{base}/">Home</a></b> |
-		<i><a class="nav-link-custom" href="{base}/about">About</a></i>
-		| <a class="nav-link-custom" href="{base}/contact">Contact</a>
-		<button
-			class="nav-custom nav-icon-custom"
-			style="font-size: 1.10rem;"
-			on:click={() => switchTheme()}><i class="fas fa-circle-half-stroke" /></button
 		>
 	</div>
 </nav>
